9341bd33984f26e613e99655a82193e6d76812e4
Matt Callaghan <mcallaghan@naperville203.org>
2024-08-26 10:28:28 -0500

    started ch 15 notes on Linked Lists

--- Chapter 15 Class Notes/src/ListDemo.java
+++ Chapter 15 Class Notes/src/ListDemo.java
@@ -9,6 +9,81 @@ public class ListDemo
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello World!");  // test again
+        System.out.println("Hello World!");
+
+        /*
+         * The addLast method can be used to populate a LinkedList.
+         */
+        LinkedList<String> staff = new LinkedList<>();
+        staff.addLast("Diana");
+        staff.addLast("Harry");
+        staff.addLast("Romeo");
+        staff.addLast("Tom");
+
+        // The list is currently: DHRT
+
+        System.out.println("staff: " + staff);
+
+        /*
+         * The listIterator method creates a new list iterator that is 
+         * positioned at the beginning of the list.
+         * The "|" in the comment represents the iterator position.
+         */
+
+         ListIterator<String> iterator = staff.listIterator();  // |DHRT
+
+         /*
+          * The next method advances the iterator over the next element in the list.
+          */
+          iterator.next();  // D|HRT
+
+          /*
+           * The next method also returns the element that the iterator
+           *    is passing.
+           */
+          String name = iterator.next();  // DH|RT
+          System.out.println(name);
+          System.out.println("Expected: Harry");
+
+          /*
+           * The add method inserts an element at the iterator position.
+           * The iterator is then positioned after the element that was added.
+           * (the text is a bit confusing on this point)
+           */
+          iterator.add("Juliet");  // DHJ|RT
+          iterator.add("Nina");  // DHJN|RT
+
+          /*
+           * The remove method removes the element returned by the last call to 
+           *   next or previous.
+           * The remove method can only be called once after calling next or previous.
+           * The remove method cannot be called after calling add.
+           */
+          iterator.next();  // DHJNR|T
+          iterator.remove();  // DHJN|T
+
+          System.out.println("staff: " + staff);
+          System.out.println("Expected: [Diana, Harry, Juliet, Nina, Tom]");
+
+          /*
+           * The set method updates the element returned by the last call to next or previous.
+           */
+          iterator.previous();  // DHJ|NT
+          iterator.set("Albert");  // DHJ|AT
+
+          /*
+           * The hasNext method is often used in the context of a while loop.
+           */
+          iterator = staff.listIterator();  // |DHJAT
+          while (iterator.hasNext())
+          {
+            String n = iterator.next();     // DHJ|AT
+            if(n.equals("Juliet")) 
+            {
+                iterator.remove();  // DH|AT
+            }
+          }   // DHAT|
+
+
     }
 }
