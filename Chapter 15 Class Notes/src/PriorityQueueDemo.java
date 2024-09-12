e4917370a63c4e5fe4ecb8ce9603f47d75ca20f4
mrcallaghan <mcallaghan@naperville203.org>
2024-09-12 10:59:16 -0500

    wrote notes on queues

--- Chapter 15 Class Notes/src/PriorityQueueDemo.java
+++ Chapter 15 Class Notes/src/PriorityQueueDemo.java
@@ -10,5 +10,10 @@ public class PriorityQueueDemo
 {
     public static void main(String[] args)
     {
+        // create a to do list using a priority queue
+        Queue<WorkOrder> q = new PriorityQueue<>();
+
+        q.add(new WorkOrder(3, "water plants"));
+        q.add(new WorkOrder(2, "make dinner"));
     }
 }
--- Chapter 15 Class Notes/src/QueueDemo.java
+++ Chapter 15 Class Notes/src/QueueDemo.java
@@ -9,5 +9,28 @@ public class QueueDemo
 {
     public static void main(String[] args)
     {
+        // create a print queue of strings (using a linked list)
+        Queue<String> jobs = new LinkedList<>();
+
+        // add several print jobs
+        jobs.add("Joe: Senior Yearbook Photos");
+        jobs.add("Molly: CS Club photo");
+
+        System.out.println("Printing...: " + jobs.remove());
+
+        jobs.add("Cathy: Sophomore photos");
+        jobs.add("Joe: Google doc");
+        jobs.add("Joe: freshman photos");
+
+        System.out.println("Printing...: " + jobs.remove());
+
+        jobs.add("Cathy: Junior photos");
+
+        // print the rest of the jobs in queue
+        while(jobs.size() > 0)
+        {
+            System.out.println("Printing: " + jobs.remove());
+        }
+
     }
 }
