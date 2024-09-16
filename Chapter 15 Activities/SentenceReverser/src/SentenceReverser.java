85e165179532bd91db561c3d54f8d2f578dba319
Matt Callaghan <mcallaghan@naperville203.org>
2024-09-16 10:30:15 -0500

    added sentence reverser solution

--- Chapter 15 Activities/SentenceReverser/src/SentenceReverser.java
+++ Chapter 15 Activities/SentenceReverser/src/SentenceReverser.java
@@ -1,28 +1,53 @@
 import java.util.Scanner;
 import java.util.Stack;
-
 /**
- * Class for reversing the order of a sentence.
+   Class for reversing the order of a sentence.
 */
 public class SentenceReverser
 {
-    /**
-     * Reverses the given sentence.
-     *
-     * @param sentence Sentence to be reversed.
-     * @return reversed sentence.
-    */
-    public static String reverse(String sentence)
-    {
-    	Scanner scanner = new Scanner(sentence);
-    	
-        // Complete this method. Use a Stack.
-        ...
-
-
-
-
-
-
-    }
+   /**
+      Reverses the given sentence.
+      @param sentence Sentence to be reversed.
+      @return reversed sentence.
+   */
+   public static String reverse(String sentence)
+   {
+      String reversed = "";
+      Scanner scanner = new Scanner(sentence);
+      Stack<String> stack = new Stack<>();
+      while (scanner.hasNext())
+      {
+         String pushWord = scanner.next();
+         stack.push(pushWord);
+         if (pushWord.contains("."))
+         {
+            boolean firstWord = true;
+            while (stack.size() > 0)
+            {
+               String popWord = stack.pop();
+               if (firstWord)
+               {
+                  String firstLetter = popWord.substring(0, 1);
+                  popWord = firstLetter.toUpperCase() + popWord.substring(1);
+                  firstWord = false;
+               }
+               else
+               {
+                  popWord = popWord.toLowerCase();
+               }
+               popWord = popWord.replace(".", "");
+               reversed += popWord;
+               if (stack.size() <= 0)
+               {
+                  reversed += ".";
+               }
+               if (scanner.hasNext() || (stack.size() > 0))
+               {
+                  reversed += " ";
+               }
+            }
+         }
+      }
+      return reversed;
+   }
 }
