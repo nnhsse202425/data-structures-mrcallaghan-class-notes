13e867c804d663ad2e4d7f8f49fe126a3a4d18ed
mrcallaghan <mcallaghan@naperville203.org>
2024-09-26 10:59:31 -0500

    finished LinkedListIteratior class

--- Chapter 16 Class Notes/src/LinkedList.java
+++ Chapter 16 Class Notes/src/LinkedList.java
@@ -69,6 +69,10 @@ public class LinkedList {
      * 
      * @return an iterator for iterating through this list
      */
+    public ListIterator listIterator()
+    {
+        return new LinkedListIterator();
+    }
 
     // Class Node
     static class Node {
@@ -76,7 +80,7 @@ public class LinkedList {
         public Node next;
     }
 
-    class LinkedListIterator // implements ListIterator
+    class LinkedListIterator implements ListIterator
     {
         // private data
         private Node position;
@@ -98,12 +102,41 @@ public class LinkedList {
          * 
          * @return the traversed element
          */
+        public Object next()
+        {
+            if(hasNext() == false)
+            {
+                throw new NoSuchElementException();
+            }
+
+            previous = position;
+            isAfterNext = true;
+            if(position == null)
+            {
+                position = first;
+            }
+            else
+            {
+                position = position.next;
+            }
+            return position.data;
+
+        }
 
         /**
          * Tests if there is an element after the iterator position.
          * 
          * @return true if there is an element after the iterator position
          */
+        public boolean hasNext()
+        {
+            if(position == null)
+        {
+            return first != null;
+        }
+        return position.next != null;
+        }
+        
 
         /**
          * Adds an element before the iterator position
@@ -111,17 +144,63 @@ public class LinkedList {
          * 
          * @param element the element to add
          */
+        public void add(Object element)
+        {
+            if (position == null)
+            {
+                addFirst(element);
+                position = first;
+            }
+            else
+            {
+                Node newNode = new Node();
+                newNode.data = element;
+                newNode.next = position.next; // #1
+                position.next = newNode;  // #2
+                position = newNode;  // #3
+            }
+
+            isAfterNext = false;  // #4
+        }
 
         /**
          * Removes the last traversed element. This method may
          * only be called after a call to the next() method.
          */
+        public void remove()
+        {
+            if(!isAfterNext)
+            {
+                throw new IllegalStateException();
+            }
+            if(position == first)
+            {
+                removeFirst();
+                position = null;
+            }
+            else
+            {
+                previous.next = position.next; // #1
+                position = previous; // #2
+
+            }
+            isAfterNext = false; // #3
+
+        }
 
         /**
          * Sets the last traversed element to a different value.
          * 
          * @param element the element to set
          */
+        public void set(Object element)
+        {
+            if(!isAfterNext)
+            {
+                throw new IllegalStateException();
+            }
+            position.data = element;
+        }
 
     }// LinkedListIterator
 }// LinkedList
--- Chapter 16 Class Notes/src/ListDemo.java
+++ Chapter 16 Class Notes/src/ListDemo.java
@@ -5,30 +5,30 @@ public class ListDemo
 {
     public static void main(String[] args)
     {
-        // LinkedList staff = new LinkedList();
-        // staff.addFirst("Tom");
-        // staff.addFirst("Romeo");
-        // staff.addFirst("Harry");
-        // staff.addFirst("Diana");
+        LinkedList staff = new LinkedList();
+        staff.addFirst("Tom");
+        staff.addFirst("Romeo");
+        staff.addFirst("Harry");
+        staff.addFirst("Diana");
 
-        // ListIterator iterator = staff.listIterator();   // |DHRT
-        // iterator.next();                                // D|HRT
-        // iterator.next();                                // DH|RT
+        ListIterator iterator = staff.listIterator();   // |DHRT
+        iterator.next();                                // D|HRT
+        iterator.next();                                // DH|RT
         
-        // // add more elements after the second element
-        // iterator.add("Juliet");                         // DHJ|RT
-        // iterator.add("Maria");                          // DHJM|RT
+        // add more elements after the second element
+        iterator.add("Juliet");                         // DHJ|RT
+        iterator.add("Maria");                          // DHJM|RT
 
-        // // remove Romeo
-        // iterator.next();                                // DHJMR|T
-        // iterator.remove();                              // DHJM|T
+        // remove Romeo
+        iterator.next();                                // DHJMR|T
+        iterator.remove();                              // DHJM|T
 
-        // // print all elements
-        // iterator = staff.listIterator();
-        // while(iterator.hasNext())
-        // {
-        //     System.out.print(iterator.next() + " ");
-        // }
-        // System.out.println();
+        // print all elements
+        iterator = staff.listIterator();
+        while(iterator.hasNext())
+        {
+            System.out.print(iterator.next() + " ");
+        }
+        System.out.println();
     }
 }
